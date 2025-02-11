d688a9dc617b49b457f54b7e2073592168679875
Agustin <agustin.canteros@gmail.com>
2025-02-11 18:51:02 -0300

    test unit

--- src/back/Test/Transport.Test2/UnitTestTransportApi.cs
+++ src/back/Test/Transport.Test2/UnitTestTransportApi.cs
@@ -1,4 +1,5 @@
 using MediatR;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Transport.Api.Controllers;
 using Transport.Api.Services;
@@ -7,35 +8,18 @@ using Transport.Repository.Repos;
 
 namespace Transport.Test2
 {
+    [TestClass]
     public class UnitTestTransportApi
     {
-        Mock<IMediator> mockMediator;
-        Mock<ITransportRepository> mockTransportRepo;
-        Mock<ITransportService> mockTransportService;
+        
 
+        
 
-        [SetUp]
-        public void Setup()
+        public UnitTestTransportApi()
         {
-            mockTransportRepo = new Mock<ITransportRepository>();
-            mockMediator = new Mock<IMediator>();
-            mockTransportService = new Mock<ITransportService>();
+            Setup();
         }
 
-        [Test]
-        public async Task Test1()
-        {
-            var res = new List<Transportt>();
-            res.Add(new Transportt() { Id = 1, License = "LIC111" });
-            res.Add(new Transportt() { Id = 2, License = "LIC112" });
-            var mockTransportContoller = new TransportController(mockTransportService.Object);
-            mockTransportService.Setup(s => s.GetByIdsAsync(It.IsAny<int[]>())).ReturnsAsync(res);
-            mockTransportRepo.Setup(s => s.GetByIdsAsync(It.IsAny<int[]>())).ReturnsAsync(res);
-            var result = await mockTransportContoller.GetByIds(It.IsAny<int[]>());
-
-            mockTransportService.Verify(v => v.GetByIdsAsync(It.IsAny<int[]>()), Times.Once);
-            mockTransportRepo.Verify(v => v.GetByIdsAsync(It.IsAny<int[]>()), Times.Once);
-            //Assert.Pass();
-        }
+        
     }
 }
---
+++ src/test/Transport.Test.Unit/Test1.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Transport.Api.Controllers;
+using Transport.Api.Services;
+using Transport.Domain.Models;
+using Transport.Repository.Repos;
+
+namespace Transport.Test.Unit
+{
+    [TestClass]
+    public sealed class TransportControllerTests
+    {
+        Mock<IMediator> mockMediator;
+        Mock<ITransportRepository> mockTransportRepo;
+        Mock<ITransportService> mockTransportService;
+
+        public TransportControllerTests()
+        {
+            Setup();
+        }
+
+        public void Setup()
+        {
+            mockTransportRepo = new Mock<ITransportRepository>();
+            mockMediator = new Mock<IMediator>();
+            mockTransportService = new Mock<ITransportService>();
+        }
+
+        [TestMethod]
+        public async Task TransportController_GetByIds_IsOk()
+        {
+            var res = new List<Transportt>();
+            res.Add(new Transportt() { Id = 1, License = "LIC111" });
+            res.Add(new Transportt() { Id = 2, License = "LIC112" });
+            var mockTransportContoller = new TransportController(mockTransportService.Object);
+            mockTransportService.Setup(s => s.GetByIdsAsync(It.IsAny<int[]>())).ReturnsAsync(res);
+            mockTransportRepo.Setup(s => s.GetByIdsAsync(It.IsAny<int[]>())).ReturnsAsync(res);
+            var result = await mockTransportContoller.GetByIds(It.IsAny<int[]>());
+            // Assert
+            mockTransportService.Verify(v => v.GetByIdsAsync(It.IsAny<int[]>()), Times.Once);
+            //mockTransportRepo.Verify(v => v.GetByIdsAsync(It.IsAny<int[]>()), Times.Once);
+            
+            Assert.IsInstanceOfType<OkObjectResult>(result);
+        }
+    }
+}
